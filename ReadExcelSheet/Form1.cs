ee3720d2508275543d0015294efafded4cd829c9
jainudi48 <jain.udit48@gmail.com>
2017-06-27 15:49:17 +0530

    Successfully read and displayed an excel sheet data

--- ReadExcelSheet/Form1.cs
+++ ReadExcelSheet/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ReadExcelSheet
 {
@@ -15,6 +16,33 @@ namespace ReadExcelSheet
         public Form1()
         {
             InitializeComponent();
+            readExcelDump();
+        }
+        public void readExcelDump()
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"/TestSheet.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            int rowCount = xlWorksheet.UsedRange.Rows.Count;
+            int colCount = xlWorksheet.UsedRange.Columns.Count;
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+
+                    //add useful things here!   
+                }
+            }
+
         }
     }
 }
